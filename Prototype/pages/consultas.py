192bba4e7e0084ef9d2c1078791ccbc6d98afec9
2Dsa3 <davsum04@hotmail.com>
2024-01-06 19:50:30 -0500

    gg

--- Prototype/pages/consultas.py
+++ Prototype/pages/consultas.py
@@ -1,257 +1,257 @@
-import dash
-from dash import html,dash_table
-import dash_bootstrap_components as dbc
-import mysql.connector
-from mysql.connector import Error
+# import dash
+# from dash import html,dash_table
+# import dash_bootstrap_components as dbc
+# import mysql.connector
+# from mysql.connector import Error
 
-# Función para crear una conexión a la base de datos
-def create_connection(host_name, user_name, user_password, db_name, db_port):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=db_name,
-            port=db_port
-        )
-        print("Connection to MySQL DB successful")
-    except Error as e:
-        print(f"The error '{e}' occurred")
-    return connection
+# # Función para crear una conexión a la base de datos
+# def create_connection(host_name, user_name, user_password, db_name, db_port):
+#     connection = None
+#     try:
+#         connection = mysql.connector.connect(
+#             host=host_name,
+#             user=user_name,
+#             passwd=user_password,
+#             database=db_name,
+#             port=db_port
+#         )
+#         print("Connection to MySQL DB successful")
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
+#     return connection
 
-# Conectar a la base de datos
-conexion = create_connection("yourminifactory.mysql.database.azure.com", "Administrador", "a5min#2023", "yourminifactory", 3306)
+# # Conectar a la base de datos
+# conexion = create_connection("yourminifactory.mysql.database.azure.com", "Administrador", "a5min#2023", "yourminifactory", 3306)
 
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
+# def execute_read_query(connection, query):
+#     cursor = connection.cursor()
+#     result = None
+#     try:
+#         cursor.execute(query)
+#         result = cursor.fetchall()
+#         return result
+#     except Error as e:
+#         print(f"The error '{e}' occurred")
 
-UsuariosyTribus_s = """
-SELECT nombre AS user, descripcion AS name_tribu
-FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu;
-"""
-UsuariosyTribus = execute_read_query(conexion, UsuariosyTribus_s)
-data_dict3 = [{"Usuario": Usuario, "Tribu": Tribu} for Usuario, Tribu in UsuariosyTribus]
-Table3 = html.Div([
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": "Usuario", "id": "Usuario"}, {"name": "Tribu", "id": "Tribu"}],
-        data=data_dict3
-        ) 
-    ])
+# UsuariosyTribus_s = """
+# SELECT nombre AS user, descripcion AS name_tribu
+# FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu;
+# """
+# UsuariosyTribus = execute_read_query(conexion, UsuariosyTribus_s)
+# data_dict3 = [{"Usuario": Usuario, "Tribu": Tribu} for Usuario, Tribu in UsuariosyTribus]
+# Table3 = html.Div([
+#     dash_table.DataTable(
+#         id='table',
+#         columns=[{"name": "Usuario", "id": "Usuario"}, {"name": "Tribu", "id": "Tribu"}],
+#         data=data_dict3
+#         ) 
+#     ])
 
-# Estilos externos
-external_stylesheets = [dbc.themes.BOOTSTRAP]
+# # Estilos externos
+# external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-# Menú desplegable
-dropdown = dbc.DropdownMenu(
-    children=[
-        dbc.DropdownMenuItem("OBJETO 3D", href="#"),
-        dbc.DropdownMenuItem("CUSTOMIZER", href="#"),
-        dbc.DropdownMenuItem("IMAGEN", href="#"),
-        dbc.DropdownMenuItem("ARTÍCULO", href="#"),
-    ],
-    nav=True,
-    in_navbar=True,
-    label="UPLOAD",
-    className="dropdown"
-)
+# # Menú desplegable
+# dropdown = dbc.DropdownMenu(
+#     children=[
+#         dbc.DropdownMenuItem("OBJETO 3D", href="#"),
+#         dbc.DropdownMenuItem("CUSTOMIZER", href="#"),
+#         dbc.DropdownMenuItem("IMAGEN", href="#"),
+#         dbc.DropdownMenuItem("ARTÍCULO", href="#"),
+#     ],
+#     nav=True,
+#     in_navbar=True,
+#     label="UPLOAD",
+#     className="dropdown"
+# )
 
-# Barra de navegación
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            #Imagen del logo
-            dbc.NavbarBrand(
-                html.Img(
-                    src='assets/Resources/logo.svg',
-                    height="30px",
-                    className="logo"
-                    ),href="/home"),
-            dbc.NavbarToggler(id="navbar-toggler"),
-            dbc.Collapse(
-                dbc.Nav(
-                    [
-                        dbc.NavItem(dbc.NavLink("TRIBES", href="/tribus")),
-                        dbc.NavItem(dbc.NavLink("TIENDA", href="/comprar")),
-                        dbc.NavItem(dbc.NavLink("CONSULTAS", href="/consultas")),
-                        dbc.NavItem(dbc.NavLink("CARRITO COMPRAS", href="/carrito_compras")),
-                    ],
-                    className="me-auto",
-                    navbar=True
-                ),
-                id="navbar-collapse",
-                navbar=True,
-                is_open=False
-            ),
-            # Formulario de búsqueda y botones de acción
-            dropdown,
-            dbc.Row(
-                [
-                    dbc.Col(dbc.NavLink("JOIN", href="/join", className="me-2"),className="sep_2"),
-                    dbc.Col(dbc.NavLink("LOGIN", href="/login"),className="sep_2"),
-                    # Icono del carrito, si lo necesitas puedes añadirlo como un dbc.NavItem
-                ],
-                align="center",
-                className="g-0 ms-auto flex-nowrap mt-3 mt-md-0 sep",
-            ),
-        ],
-        fluid=True,  # Establece el contenedor como fluido para que ocupe todo el ancho
-        className="Container_header"
-    ),
-    color="light",
-    dark=False,
-    sticky="top",
-    expand="lg",  # Asegúrate de que la barra de navegación se expanda en pantallas grandes
-    className="Container_header"
-)
+# # Barra de navegación
+# navbar = dbc.Navbar(
+#     dbc.Container(
+#         [
+#             #Imagen del logo
+#             dbc.NavbarBrand(
+#                 html.Img(
+#                     src='assets/Resources/logo.svg',
+#                     height="30px",
+#                     className="logo"
+#                     ),href="/home"),
+#             dbc.NavbarToggler(id="navbar-toggler"),
+#             dbc.Collapse(
+#                 dbc.Nav(
+#                     [
+#                         dbc.NavItem(dbc.NavLink("TRIBES", href="/tribus")),
+#                         dbc.NavItem(dbc.NavLink("TIENDA", href="/comprar")),
+#                         dbc.NavItem(dbc.NavLink("CONSULTAS", href="/consultas")),
+#                         dbc.NavItem(dbc.NavLink("CARRITO COMPRAS", href="/carrito_compras")),
+#                     ],
+#                     className="me-auto",
+#                     navbar=True
+#                 ),
+#                 id="navbar-collapse",
+#                 navbar=True,
+#                 is_open=False
+#             ),
+#             # Formulario de búsqueda y botones de acción
+#             dropdown,
+#             dbc.Row(
+#                 [
+#                     dbc.Col(dbc.NavLink("JOIN", href="/join", className="me-2"),className="sep_2"),
+#                     dbc.Col(dbc.NavLink("LOGIN", href="/login"),className="sep_2"),
+#                     # Icono del carrito, si lo necesitas puedes añadirlo como un dbc.NavItem
+#                 ],
+#                 align="center",
+#                 className="g-0 ms-auto flex-nowrap mt-3 mt-md-0 sep",
+#             ),
+#         ],
+#         fluid=True,  # Establece el contenedor como fluido para que ocupe todo el ancho
+#         className="Container_header"
+#     ),
+#     color="light",
+#     dark=False,
+#     sticky="top",
+#     expand="lg",  # Asegúrate de que la barra de navegación se expanda en pantallas grandes
+#     className="Container_header"
+# )
 
-Ventas_por_Año_S= """
-SELECT YEAR(fecha_pago) as Anio, SUM(total) as IngresoTotal
-FROM carro_compra
-GROUP BY YEAR(fecha_pago)
-ORDER BY Anio DESC;
-"""
-Campañas_dinerosRecaudados_S = """
-SELECT descricipcion, sum(dinerorecaudado) as total
-FROM campana 
-GROUP BY descricipcion
-ORDER BY total DESC;
-"""
+# Ventas_por_Año_S= """
+# SELECT YEAR(fecha_pago) as Anio, SUM(total) as IngresoTotal
+# FROM carro_compra
+# GROUP BY YEAR(fecha_pago)
+# ORDER BY Anio DESC;
+# """
+# Campañas_dinerosRecaudados_S = """
+# SELECT descricipcion, sum(dinerorecaudado) as total
+# FROM campana 
+# GROUP BY descricipcion
+# ORDER BY total DESC;
+# """
 
-Ventas_por_Año = execute_read_query(conexion, Ventas_por_Año_S)
-Campañas_dinerosRecaudados = execute_read_query(conexion, Campañas_dinerosRecaudados_S)
+# Ventas_por_Año = execute_read_query(conexion, Ventas_por_Año_S)
+# Campañas_dinerosRecaudados = execute_read_query(conexion, Campañas_dinerosRecaudados_S)
 
-data_dict = [{"Año": year, "Ingreso": income} for year, income in Ventas_por_Año]
-Table = html.Div([
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": "Año", "id": "Año"}, {"name": "Ingreso", "id": "Ingreso"}],
-        data=data_dict
-    )
-])
-data_dict2 = [{"Campaña": Campaña, "Total": Total} for Campaña, Total in Campañas_dinerosRecaudados]
-Table2 = html.Div([
-    dash_table.DataTable(
-        id='table',   
-        columns=[{"name": "Campaña", "id": "Campaña"}, {"name": "Total", "id": "Total"}],
-        data=data_dict2,
-        style_cell={'textAlign': 'left'},
-        style_data_conditional=[
-            {
-                'if': {'column_id': 'descricipcion'},
-                'fontSize': 20,  # Tamaño de la fuente de la columna "Campaña"
-                'fontWeight': 'bold'  # Poner el texto en negrita
-            }
-        ]
-    )
-])
+# data_dict = [{"Año": year, "Ingreso": income} for year, income in Ventas_por_Año]
+# Table = html.Div([
+#     dash_table.DataTable(
+#         id='table',
+#         columns=[{"name": "Año", "id": "Año"}, {"name": "Ingreso", "id": "Ingreso"}],
+#         data=data_dict
+#     )
+# ])
+# data_dict2 = [{"Campaña": Campaña, "Total": Total} for Campaña, Total in Campañas_dinerosRecaudados]
+# Table2 = html.Div([
+#     dash_table.DataTable(
+#         id='table',   
+#         columns=[{"name": "Campaña", "id": "Campaña"}, {"name": "Total", "id": "Total"}],
+#         data=data_dict2,
+#         style_cell={'textAlign': 'left'},
+#         style_data_conditional=[
+#             {
+#                 'if': {'column_id': 'descricipcion'},
+#                 'fontSize': 20,  # Tamaño de la fuente de la columna "Campaña"
+#                 'fontWeight': 'bold'  # Poner el texto en negrita
+#             }
+#         ]
+#     )
+# ])
 
-UsuariosyTribus_s = """
-SELECT nombre AS user, descripcion AS name_tribu
-FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu;
-"""
-UsuariosyTribus = execute_read_query(conexion, UsuariosyTribus_s)
-data_dict3 = [{"Usuario": Usuario, "Tribu": Tribu} for Usuario, Tribu in UsuariosyTribus]
-Table3 = html.Div([
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": "Usuario", "id": "Usuario"}, {"name": "Tribu", "id": "Tribu"}],
-        data=data_dict3
-        ) 
-    ])
+# UsuariosyTribus_s = """
+# SELECT nombre AS user, descripcion AS name_tribu
+# FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu;
+# """
+# UsuariosyTribus = execute_read_query(conexion, UsuariosyTribus_s)
+# data_dict3 = [{"Usuario": Usuario, "Tribu": Tribu} for Usuario, Tribu in UsuariosyTribus]
+# Table3 = html.Div([
+#     dash_table.DataTable(
+#         id='table',
+#         columns=[{"name": "Usuario", "id": "Usuario"}, {"name": "Tribu", "id": "Tribu"}],
+#         data=data_dict3
+#         ) 
+#     ])
 
-accordion = html.Div([
-    html.H1("Default Queries"),
-    dbc.Accordion(
-        [
-            dbc.AccordionItem(
-                [
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                Table, className="Margin-left"
-                            ),
-                            dbc.Col(
-                                html.Div(
-                                    html.Pre(
-                                        """
-                                        SELECT YEAR(fecha_pago) as Anio, SUM(total) as IngresoTotal
-                                        FROM carro_compra
-                                        GROUP BY YEAR(fecha_pago)
-                                        ORDER BY Anio DESC;
-                                        """
-                                    ),
-                                ),className="Margin-right"
-                            )
-                        ],
-                        justify="center",
-                        className="Container_center"
-                    )
-                ],
-                title="Ventas por año",
-            ),
-            dbc.AccordionItem(
-                [
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                Table2, className="Margin-left"
-                            ),
-                            dbc.Col(
-                                html.Div(
-                                    html.Pre(
-                                        """
-                                            SELECT descricipcion, sum(dinerorecaudado) as total
-                                            FROM campana 
-                                            GROUP BY descricipcion
-                                            ORDER BY total DESC; """
-                                    ),
-                                ),className="Margin-right"
-                            )
-                        ],
-                        justify="center",
-                        className="Container_center"
-                    )
-                ],
-                title="Dinero Recauado por Compaña",
-            ),
-            dbc.AccordionItem(
-                [
-                    dbc.Row(
-                        [
-                            dbc.Col(
-                                Table3, className="Margin-left"
-                            ),
-                            dbc.Col(
-                                html.Div(
-                                    html.Pre(
-                                        """
-                                            SELECT nombre AS Usuario, descripcion AS Tribu
-                                            FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu; """
-                                    ),
-                                ),className="Margin-right"
-                            )
-                        ],
-                        justify="center",
-                        className="Container_center"
-                    )
-                ],
-                title="Usuarios y las tribus a las que pertenecen",
-            ),
-        ],
-    )],className="Container_center_c"
-)
+# accordion = html.Div([
+#     html.H1("Default Queries"),
+#     dbc.Accordion(
+#         [
+#             dbc.AccordionItem(
+#                 [
+#                     dbc.Row(
+#                         [
+#                             dbc.Col(
+#                                 Table, className="Margin-left"
+#                             ),
+#                             dbc.Col(
+#                                 html.Div(
+#                                     html.Pre(
+#                                         """
+#                                         SELECT YEAR(fecha_pago) as Anio, SUM(total) as IngresoTotal
+#                                         FROM carro_compra
+#                                         GROUP BY YEAR(fecha_pago)
+#                                         ORDER BY Anio DESC;
+#                                         """
+#                                     ),
+#                                 ),className="Margin-right"
+#                             )
+#                         ],
+#                         justify="center",
+#                         className="Container_center"
+#                     )
+#                 ],
+#                 title="Ventas por año",
+#             ),
+#             dbc.AccordionItem(
+#                 [
+#                     dbc.Row(
+#                         [
+#                             dbc.Col(
+#                                 Table2, className="Margin-left"
+#                             ),
+#                             dbc.Col(
+#                                 html.Div(
+#                                     html.Pre(
+#                                         """
+#                                             SELECT descricipcion, sum(dinerorecaudado) as total
+#                                             FROM campana 
+#                                             GROUP BY descricipcion
+#                                             ORDER BY total DESC; """
+#                                     ),
+#                                 ),className="Margin-right"
+#                             )
+#                         ],
+#                         justify="center",
+#                         className="Container_center"
+#                     )
+#                 ],
+#                 title="Dinero Recauado por Compaña",
+#             ),
+#             dbc.AccordionItem(
+#                 [
+#                     dbc.Row(
+#                         [
+#                             dbc.Col(
+#                                 Table3, className="Margin-left"
+#                             ),
+#                             dbc.Col(
+#                                 html.Div(
+#                                     html.Pre(
+#                                         """
+#                                             SELECT nombre AS Usuario, descripcion AS Tribu
+#                                             FROM usuario JOIN tribu ON usuario.id_tribu = tribu.id_tribu; """
+#                                     ),
+#                                 ),className="Margin-right"
+#                             )
+#                         ],
+#                         justify="center",
+#                         className="Container_center"
+#                     )
+#                 ],
+#                 title="Usuarios y las tribus a las que pertenecen",
+#             ),
+#         ],
+#     )],className="Container_center_c"
+# )
 
-# Agregar estilos CSS para asegurar que no haya margen o relleno afectando al navbar
-layout = html.Div([
-    navbar,accordion
-], style={'margin': '0', 'padding': '0'})
+# # Agregar estilos CSS para asegurar que no haya margen o relleno afectando al navbar
+# layout = html.Div([
+#     navbar,accordion
+# ], style={'margin': '0', 'padding': '0'})
 
--- Prototype/pages/pay.py
+++ Prototype/pages/pay.py
@@ -3,6 +3,7 @@ from dash import html
 import dash_bootstrap_components as dbc
 from dash import html, dcc, Input, Output, State, callback,dash
 from dash.dependencies import MATCH
+import pages.carrito_compras as carrito_compras
 
 # Estilos externos
 external_stylesheets = [dbc.themes.BOOTSTRAP]
@@ -70,99 +71,46 @@ navbar = dbc.Navbar(
     expand="lg",  # Asegúrate de que la barra de navegación se expanda en pantallas grandes
     className="Container_header"
 )
-tribe_options = {
-    "Silver": {
-        "price": "Include VAT/Sales Tax (if applicable)",
-        "features": [
-            "Welcome Package",
-            "Monthly Mini with Pre-Supported & Test-Printed STLs",
-            "Illustrated Tabletop Roleplaying Stat Sheet",
-            "Monthly one-month free trial membership invitation to one (or more!) small/new/upcoming MMF Tribe via the Creator Boost program!"
-        ],
-        "description": "Get a brand new, ultra-high detail 3x3 or larger 'Miniature' STL, and access to our Creator Boost program every month!"
-    },
-    "Gold": {
-        "price": "Include VAT/Sales Tax (if applicable)",
-        "features": [
-            "Welcome Package",
-            "Monthly Mini with Pre-Supported & Test-Printed STLs",
-            "Oversized Bust STL of Monthly Mini",
-            "Illustrated Tabletop Roleplaying Stat Sheet",
-            "Monthly one-month free trial membership invitation to one (or more!) small/new/upcoming MMF Tribe via the Creator Boost program!",
-            "50% Store Discount (found in pinned post)",
-            "4 (or more!) Bonus Reward 'Miniatures' + Oversized Busts Yearly"
-        ],
-        "description": "Get a brand new, ultra-high detail 3x3 or larger 'Miniature' STL every month and their oversized Bust, access to our Creator Boost program, a 50% discount on our Store, and access to Seasonal and Bonus Rewards!"
-    },
-    "Platinum": {
-        "price": "Include VAT/Sales Tax (if applicable)",
-        "features": [
-            "Welcome Package",
-            "Monthly Mini with Pre-Supported & Test-Printed STLs",
-            "Oversized Bust STL of Monthly Mini",
-            "Illustrated Tabletop Roleplaying Stat Sheet",
-            "Monthly one-month free trial membership invitation to one (or more!) small/new/upcoming MMF Tribe via the Creator Boost program!",
-            "50% Store Discount (found in pinned post)",
-            "4 (or more!) Bonus Reward 'Miniatures' + Oversized Busts Yearly"
-        ],
-        "description": "Get a brand new, ultra-high detail 3x3 or larger 'Miniature' STL every month and their oversized Bust, access to our Creator Boost program, a 50% discount on our Store, and access to Seasonal and Bonus Rewards! Plus help Witchsong/The Witchguild grow and assist the 3D-printing community!"
-    }
-}
-
-def create_tribe_card(title, details):
-    return dbc.Card(
-        [
-            dbc.CardHeader(title, className="text-center"),
-            dbc.CardBody(
-                [
-                    html.P(details["price"], className="card-text"),
-                    html.P(details["description"], className="card-text"),
-                    html.Hr(),
-                    html.H5("Items you will have access to:", className="card-title"),
-                    html.Ul([html.Li(feature) for feature in details["features"]]),
-                    dbc.Button("Join", color="primary", id=f"type-{title}"),
-                ]
-            ),
-        ],
-        className="m-2"
-    )
-
-tribes_section = dbc.Container(
-    [
-        dbc.Row(dbc.Col(html.H1("Join the Tribe"), width={"size": 6, "offset": 3}, className="text-center")),
-        dbc.Row(
-            [
-                html.Div(id='join-message', style={'text-align': 'center'})
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(create_tribe_card("Silver", tribe_options["Silver"]), width=4),
-                dbc.Col(create_tribe_card("Gold", tribe_options["Gold"]), width=4),
-                dbc.Col(create_tribe_card("Platinum", tribe_options["Platinum"]), width=4),
+total = carrito_compras.getTotalAmount()
+pay_section = html.Div([
+    html.H2('Secure Payment', style={'textAlign': 'center'}),
+    html.Div([
+        html.Div([
+            html.Label('Card Number'),
+            dcc.Input(placeholder='Enter card number...', type='text')
+        ], style={'padding': '10px'}),
+        html.Div([
+            html.Label('Expiry Date'),
+            dcc.Input(placeholder='MM/YY', type='text')
+        ], style={'padding': '10px'}),
+        html.Div([
+            html.Label('CVV'),
+            dcc.Input(placeholder='CVV', type='text')
+        ], style={'padding': '10px'}),
+        html.Div([
+            html.Label('Cardholder Name'),
+            dcc.Input(placeholder='Enter cardholder name...', type='text')
+        ], style={'padding': '10px'})
+    ], style={'display': 'flex', 'flexDirection': 'column'}),
+    html.Div([
+        dcc.Dropdown(
+            options=[
+                {'label': 'Visa', 'value': 'VISA'},
+                {'label': 'MasterCard', 'value': 'MC'},
+                # Add other card types here
             ],
-            className="mb-4",
-        ),
-    ],
-    fluid=True
-)
-# Agregar estilos CSS para asegurar que no haya margen o relleno afectando al navbar
-# Placeholder para el mensaje
-
-
-@callback(
-    Output('join-message', 'children'),
-    Input("type-Silver", 'n_clicks'),
-    Input("type-Gold", 'n_clicks'),
-    Input("type-Platinum", 'n_clicks'),
-    prevent_initial_call=True
-)
-def on_join_click(silver_clicks, gold_clicks, platinum_clicks):
-    if silver_clicks or gold_clicks or platinum_clicks:
-        return f"Success in your new tribe !! "
+            placeholder='Select card type'
+        )
+    ], style={'padding': '10px'}),
+    html.Div([
+        html.Label('Total Amount: ' + str(total) + ' USD')
+    ], style={'padding': '10px', 'fontWeight': 'bold'}),
+    html.Button('Pay Now', id='pay-button', n_clicks=0, style={'width': '100%', 'padding': '10px'})
+], style={'width': '400px', 'margin': '0 auto', 'boxShadow': '0px 0px 5px #ccc', 'padding': '20px'})
 
 # Agrega el placeholder al layout
 layout = html.Div([
     navbar,
-    tribes_section,
+    pay_section,
 ], style={'margin': '0', 'padding': '0'})
+
